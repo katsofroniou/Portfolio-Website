ae45924f09fb4d8810b9615a965cd77d4a418dd1
Katerina Sofroniou <katerina.sof@btinternet.com>
2023-05-14 19:44:20 +0100

    wip: added info w/o formatting

--- portfolio/src/routes/about/+page.svelte
+++ portfolio/src/routes/about/+page.svelte
@@ -32,8 +32,77 @@
 <main>
 	<h1>About Me</h1>
 
-	<p class="name">Katerina Sofroniou</p>
-	<p class="dob">{myAge}</p>
+	<div class="name-age">
+		<p class="name">Katerina Sofroniou, {myAge}</p>
+	</div>
+
+	<div class="edu-vol">
+		<h3>Education and Volunteering</h3>
+		
+		<h5>Education</h5>
+		<ul>
+			<li>Royal Holloway 2021-2024</li>
+			<li>Computer Science Bsc</li>
+		</ul>
+		
+		<h5>Volunteering</h5>
+		<p> University </p>
+		<ul>
+			<li>Anime and Manga Society - Diversity and Inclusivity Rep 2023-2023</li>
+			<li>Anime and Manga Society - Treasurer 2023-2024</li>
+			<li>Peer Mentor</li>
+		</ul>
+
+		<p> Secondary School </p>
+		<ul>
+			<li>Digital Leader</li>
+			<li>Robotics Club Volunteer</li>
+			<li>Prefect</li>
+		</ul>
+
+		<h5>Other Achievements</h5>
+		<ul>
+			<li>Grade 3 Piano - 2016</li>
+			<li>Level 3 Algebra Award - 2017</li>
+			<li>Brilliant Club 1st in Neuroscience - 2018</li>
+			<li>Brilliant Club 1st in Bayesian Statistics - 2019</li> 
+		</ul>
+	</div>
+
+	<div class="skills">
+		<h3>Skills</h3>
+		<h5>Languages</h5>
+		<ul>
+			<li>Python</li>
+			<li>Java</li>
+			<li>SQL</li>
+			<li>HTML - CSS - Javascript - Typescript</li>
+			<li>Prolog</li>
+		</ul>
+		<h5>Technology</h5>
+		<ul>
+			<li>Web Frameworks - React, Svelte, Vite</li>
+			<li>Django</li>
+			<li>Git</li>
+			<li>PostgreSQL</li>
+		</ul>
+		<h5>Other Skills</h5>
+		<ul>
+			<li>Linux</li>
+			<li>Linear Algebra - Multi Dimensional Data Processing</li>
+			<li>Algorithms & Complexity</li>
+			<li>Data Analysis</li>
+		</ul>
+	</div>
+
+	<div class="hobbies">
+		<h3>Hobbies</h3>
+		<ul>
+			<li>Gaming</li>
+			<li>Painting and Drawing</li>
+			<li>Playing Piano</li>
+		</ul>
+	</div>
 </main>
 
 <style>
