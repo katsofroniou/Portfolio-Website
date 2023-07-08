5c8e1866d0e0f32f20de2bda572babbcdc6fca8d
Katerina Sofroniou <katerina.sof@btinternet.com>
2023-07-08 22:27:42 +0100

    feat: added basics for cards

---
+++ portfolio/src/routes/projects/+page.svelte
@@ -0,0 +1,34 @@
+<script lang="ts"></script>
+
+<svelte:head>
+	<title>About Me</title>
+	<meta name="About Me" content="About" />
+	<style type="text/css">
+		body {
+			font-family: 'Lato', sans-serif;
+			background-color: #ffe3f1;
+			margin: 0;
+			padding: 20px;
+		}
+	</style>
+</svelte:head>
+
+<main>
+    <div class="projectsContainer">
+        <div class="project">1</div>
+        <div class="project">2</div>
+        <div class="project">3</div>
+        <div class="project">4</div>
+        <div class="project">5</div>
+        <div class="project">6</div>
+    </div>
+</main>
+
+<style lang="scss">
+
+    .projectsContainer {
+        display: flex;
+        flex-direction: row;
+    }
+
+</style>
