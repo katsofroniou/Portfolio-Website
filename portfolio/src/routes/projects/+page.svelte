9a9e544e83ee60324f91e419d818d8c74eff2d39
Katerina Sofroniou <katerina.sof@btinternet.com>
2023-07-08 22:33:26 +0100

    fix: broken projects route fixed

---
+++ portfolio/src/routes/projects/+page.svelte
@@ -0,0 +1,21 @@
+<script>
+</script>
+
+<svelte:head>
+	<title>Under Development</title>
+	<style type="text/css">
+		body {
+			background-color: rgb(255, 227, 241);
+		}
+	</style>
+</svelte:head>
+
+<main>
+    <h1> Under Development </h1>
+</main>
+
+<style>
+    h1 {
+        text-align: center;
+    }
+</style>
