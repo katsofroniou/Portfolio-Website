5fd9b4b76fe2ed90bc954936907ad5ea19a2ada5
katsofroniou <katerina.sof@btinternet.com>
2023-09-06 22:20:15 +0100

    feat: made basic header

---
+++ portfolio/src/components/Header.svelte
@@ -0,0 +1,31 @@
+<script lang="ts">
+    // Components
+    import NavBar from "./NavBar.svelte";
+</script>
+
+<main class="header">
+    <div class="top">
+        <div class="name">
+            <h3>Kat Sofroniou</h3>
+        </div>
+        <div class="dl-toggle">
+            <button class="theme-button">dl</button>
+        </div>
+    </div>
+    <div class="bottom">
+        <div class="email">
+            <a href="mailto:katerina@strawberrykat.co.uk"><button>email</button></a>
+        </div>
+        <div class="social">
+            <p>Github</p>
+            <p>LinkedIn</p>
+        </div>
+    </div>
+
+    <NavBar/>
+</main>
+
+<style lang="scss">
+    @import '../styles/header.scss';
+</style>
+
